422575edec74f605dd5abce62b7354dbedc3b671
Andy Castille <me@andycc.dev>
2021-05-04 19:14:21 -0500

    move camera up

--- Quest Engine/Camera.cpp
+++ Quest Engine/Camera.cpp
@@ -3,7 +3,7 @@
 
 Camera::Camera()
 {
-	m_position = glm::vec3(0.0f, 0.0f, 0.0f);
+	m_position = glm::vec3(0.0f, 3.0f, 0.0f);
 	m_forward = glm::vec3(0.0f, 0.0f, -1.0f);
 	m_up = glm::vec3(0.0f, 1.0f, 0.0f);
 	m_right = glm::vec3(1.0f, 0.0f, 0.0f);
